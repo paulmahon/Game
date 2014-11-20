5a81a72bcca069f2eee3807879ea4f839c2646ee
paulmahon <x14119145@student.ncirl.ie>
2014-11-20 22:51:33 +0000

    Initial Files

---
+++ Game.java
@@ -0,0 +1,34 @@
+/*
+ *	Game.java
+ *  Instantiable Class
+ *	@author Paul Mahon
+ *	Student ID: 14119145
+ *	20/11/14
+ */
+
+
+public class Game{
+	//set data members or varibles (They must be private)
+	//private
+
+	//constructor (initalise varibles)
+	public Game(){
+
+	}
+
+	//set methods - one for every input
+	//public void
+		//this.
+
+	//compute - process
+	public void compute(){
+
+	}
+
+	//get methods - one for every output
+	//public int getSomthing(){
+	//	return something;
+	}
+
+
+}
---
+++ GameApp.java
@@ -0,0 +1,42 @@
+/*
+ *	GameApp.java
+ *	@author Paul Mahon
+ *	student ID 14119145
+ *	@date 20/11/14
+ */
+
+
+import java.util.Scanner;
+
+public class GameApp{
+	public static void main(String arg[]){
+		//declare variables
+		//int
+		// string
+
+
+		//declare objects
+		Scanner sc;
+		//Game myGame;
+
+		//create objects
+		sc = new Scanner(System.in);
+		//myGame = new Game();
+
+		//ask for input
+		System.out.println("bla bla");
+		xxx = sc.
+		//take input
+
+		//set
+
+		//compute/process
+		myGame.compute();
+
+		//get
+
+		//output
+		yyy = myGame.getwww();
+		system.out.println("bla bla");
+	}
+}
